00032c01f556fcaf91c7dc4d5b09cd1c6c438a6c
めいそん <jnhst443@yahoo.co.jp>
2012-06-03 14:43:30 +0900

    jit 実験コード追加

--- jit/jit3.py
+++ jit/jit3.py
@@ -8,10 +8,11 @@ MEM_RELEASE  = 0x8000
 PAGE_EXECUTE_READWRITE = 0x40
 
 codes = (c_ubyte * 32)(
-    0x6a, 0x41,                     # push 65
-    0xe8, 0x00, 0x00, 0x00, 0x00,   # call xxxx
-    0x83, 0xc4, 0x04,               # add esp, 4
-    0xc3,                           # ret
+    0x6a, 0x41,                   # push 65
+    0xb8, 0x00, 0x00, 0x00, 0x00, # mov eax, 0
+    0xff, 0xd0,                   # call eax
+    0x83, 0xc4, 0x04,             # add esp, 4
+    0xc3,                         # ret
 )
 
 buflen = len(codes)
@@ -20,7 +21,7 @@ p = VirtualAlloc(0, buflen, MEM_COMMIT, PAGE_EXECUTE_READWRITE)
 getaddr = CFUNCTYPE(c_void_p, c_void_p)(lambda p: p)
 putchar = cdll.msvcrt.putchar
 
-codes[3:7] = map(lambda a: ord(a), pack("<L", getaddr(putchar) - (p + 7)))
+codes[3:7] = map(lambda a: ord(a), pack("<L", getaddr(putchar)))
 memmove(p, addressof(codes), buflen)
 
 f = CFUNCTYPE(None)(p)
